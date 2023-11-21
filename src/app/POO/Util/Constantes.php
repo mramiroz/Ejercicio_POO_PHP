d786a53ab6e7c2315914ff888d63153931681064
mramiroz <manuelramzab@gmail.com>
2023-11-21 12:04:55 +0100

    Falta el diagrama de navegacion

--- src/app/POO/Util/Constantes.php
+++ src/app/POO/Util/Constantes.php
@@ -3,7 +3,7 @@
 	include_once "vendor/autoload.php";
 	class Constantes{
 		const CP_CANARIAS = ["35", "38"];
-		const CP_FUERA_PENINSULA = ["07", "51", "52"];
+		const CP_FUERA_PENINSULA = ["07", "51", "52", "35", "38"];
 		const PRECIO_TRANSPORTE = 100;
 		const RECARGO_FUERA_PENINSULA = 1.05;
 		const DESCUENTO_CANARIAS = 0.80;
